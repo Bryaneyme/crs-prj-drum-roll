2832e645ccffd2d238a38761b691321e1d855fe5
Bryan <beyme.pro@gmail.com>
2025-03-05 23:26:41 +0700

    Basic active state animation
    Project done

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,4 +1,5 @@
 let allButtons = document.querySelectorAll(".drum");
+
 const audioFiles = {
     "w": "./sounds/crash.mp3",
     "a": "./sounds/kick-bass.mp3",
@@ -9,30 +10,44 @@ const audioFiles = {
     "l": "./sounds/tom-4.mp3"
 };
 
-// CLICK: Event Listeners
+// eventListener: click
 allButtons.forEach(button => {
     button.addEventListener('click', function (buttonEvent) {
-        playAudio(buttonEvent, button)
+        playAudio(buttonEvent, button);
+        animateButton(buttonEvent, button);
     });
 });
 
-// KEYPRESS: Event listeners
+// eventListener: keyPress
 document.addEventListener('keydown', function (buttonEvent) {
-    playAudio(buttonEvent, null)
+    playAudio(buttonEvent, null);
+    animateButton(buttonEvent, null);
 })
 
-// PLAY AUDIO: Function
 function playAudio(buttonEvent, button) {
     let key;
-    if (button) {key = button.textContent}
+    if (button) { key = button.textContent }
 
-    else {key = buttonEvent.key}
+    else { key = buttonEvent.key }
 
     if (key in audioFiles) {
         new Audio(audioFiles[key]).play();
-        console.log(`Played ${key} sound. Path: ${audioFiles[key]}`)
+        console.log(`Played ${key} sound. Path: ${audioFiles[key]}`);
     }
     else {
-        console.log("Unable to play button sound")
+        console.log("Unable to play button sound");
     }
+}
+
+function animateButton(buttonEvent, button) {
+    let activeButton;
+    if (button) { activeButton = button }
+
+    else { activeButton = document.querySelector(`.${buttonEvent.key}`) }
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed")
+    }, 300)
 }
