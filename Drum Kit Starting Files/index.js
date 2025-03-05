854c7059455e9930668b41fc4916a01619dbec05
Bryan <beyme.pro@gmail.com>
2025-03-05 22:57:33 +0700

    Moved audioFiles (sound paths) out of function

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,4 +1,13 @@
 let allButtons = document.querySelectorAll(".drum");
+const audioFiles = {
+    "w": "./sounds/crash.mp3",
+    "a": "./sounds/kick-bass.mp3",
+    "s": "./sounds/snare.mp3",
+    "d": "./sounds/tom-1.mp3",
+    "j": "./sounds/tom-2.mp3",
+    "k": "./sounds/tom-3.mp3",
+    "l": "./sounds/tom-4.mp3"
+};
 
 // CLICK: Event Listeners
 allButtons.forEach(button => {
@@ -18,16 +27,6 @@ function playAudio(buttonEvent, button) {
     if (button) {key = button.textContent}
 
     else {key = buttonEvent.key}
-
-    const audioFiles = {
-        "w": "./sounds/crash.mp3",
-        "a": "./sounds/kick-bass.mp3",
-        "s": "./sounds/snare.mp3",
-        "d": "./sounds/tom-1.mp3",
-        "j": "./sounds/tom-2.mp3",
-        "k": "./sounds/tom-3.mp3",
-        "l": "./sounds/tom-4.mp3"
-    };
 
     if (key in audioFiles) {
         new Audio(audioFiles[key]).play();
